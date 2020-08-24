591d536c6f1c33f17297f176d0d8f686cd33a7b4
Nathan Smith <44684438+NateTheGreat123@users.noreply.github.com>
2020-08-24 16:51:28 -0700

    helloworld2

---
+++ hello2.cpp
@@ -0,0 +1,40 @@
+// Author: Riley Kuo & Nathan Smith
+// Description: Hello World in C++
+//============================================================
+
+#include <iostream>
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+
+int main()
+{
+    string name1 = "your name1";
+    hello1Function(name1);
+    string name2 = "your name2";
+    //hello2Function(name2);
+    return 0;
+}
+
+/*
+* hello1Function.cpp
+*
+*/
+#include <iostream>
+using namespace std;
+int hello1Function(string name1)
+{
+    cout << "hello World " << name1 << endl;
+    return 0;
+}
+/*
+* hello2Function.cpp
+*
+*/
+#include <iostream>
+using namespace std;
+int hello2Function(string name2)
+{
+    cout << "hello World " << name2 << endl;
+    return 0;
+}
